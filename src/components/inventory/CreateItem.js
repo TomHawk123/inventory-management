efc2ff183967c7e7926ab4be4d98da20c8713d31
Matthew McMorries <matthew.mcmorries@gmail.com>
2022-03-17 20:19:39 -0500

    button renders and redirects correctly. Form not rendering

---
+++ src/components/inventory/CreateItem.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export const Item = () => {
+    
+    return (
+        <>
+        <h2>Item Details</h2>
+        <section className="item">
+            <h3 className="item__description"></h3>
+            <div className="Item__"></div>
+            <div className="Item__"></div>
+        </section>        
+        </>
+    )
+}
