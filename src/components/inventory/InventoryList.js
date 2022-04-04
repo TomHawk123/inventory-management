3063ce77cb6a6f5d54537ab5216ede476136b2fe
Matthew McMorries <matthew.mcmorries@gmail.com>
2022-04-04 13:23:28 -0500

    failed at api cleanup. heh

--- src/components/inventory/InventoryList.js
+++ src/components/inventory/InventoryList.js
@@ -1,152 +1,137 @@
-import React, { useEffect, useState } from "react"
-import { useHistory } from "react-router-dom"
-import { fetchInventory, fetchUserInventory, sendUserItem } from "../ApiManager"
-import "./Inventory.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus, faTrash, faPencil } from '@fortawesome/free-solid-svg-icons'
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import {
+    fetchInventory,
+    fetchUserInventory,
+    sendUserItem,
+} from "../ApiManager";
+import "./Inventory.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faTrash, faPencil } from "@fortawesome/free-solid-svg-icons";
 
 // export a function that will return the HTML
 export const InventoryList = () => {
-    const history = useHistory()
+    const history = useHistory();
     // set up variables for application state with useState hook
-    const [inventoryList, setInventoryList] = useState([])
-    const [userInventory, setUserInventory] = useState([])
-    const [totalItemsMessage, updateMessage] = useState('')
+    const [inventoryList, setInventoryList] = useState([]);
+    const [userInventory, setUserInventory] = useState([]);
+    const [totalItemsMessage, updateMessage] = useState("");
 
     // fetch inventory list when inventory state changes
     useEffect(
         () => {
-            fetchInventory()
-                .then(inventoryArray => {
-                    setInventoryList(inventoryArray)
-                })
-            fetchUserInventory()
-                .then(userInventoryArray => {
-                    setUserInventory(userInventoryArray)
-                })
+            fetchInventory().then((inventoryArray) => {
+                setInventoryList(inventoryArray);
+            });
+            fetchUserInventory().then((userInventoryArray) => {
+                setUserInventory(userInventoryArray);
+            });
         },
         [] // Only run when initial JSX rendering is complete.
-    )
+    );
 
-    useEffect(
-        () => {
-            if (inventoryList.length === 1) {
-                updateMessage("You have 1 inventory item")
-            } else {
-                updateMessage(`You have ${inventoryList.length} inventory line items`)
-            }
-        },
-        [inventoryList]
-    )
+    useEffect(() => {
+        if (inventoryList.length === 1) {
+            updateMessage("You have 1 inventory item");
+        } else {
+            updateMessage(`You have ${inventoryList.length} inventory line items`);
+        }
+    }, [inventoryList]);
 
     const deleteItem = (id) => {
         fetch(`http://localhost:8088/inventories/${id}`, {
-            method: "DELETE"
-        }).then(fetchInventory)
-            .then(inventoryArray => {
-                setInventoryList(inventoryArray)
-            })
-    }
-
+            method: "DELETE",
+        })
+            .then(fetchInventory)``
+            .then((inventoryArray) => {
+                setInventoryList(inventoryArray);
+            });
+    };
 
     return (
         <>
-
             <div id="totalItemsMessage">{totalItemsMessage}</div>
 
-            {localStorage.getItem("inventory__admin") ?
-
-                <button className="inventoryListCreateButton"
-                    onClick={
-                        () => history.push("/inventory/create")}>Create New
+            {localStorage.getItem("inventory__admin") ? (
+                <button
+                    className="inventoryListCreateButton"
+                    onClick={() => history.push("/inventory/create")}
+                >
+                    Create New
                 </button>
-
-                : null
-            }
+            ) : null}
 
             <div id="centerpiece">
-                {
-                    inventoryList.map(
-                        inventoryObject => <div key={`deleteItem--${inventoryObject.id}`} className="inventory__List"
+                {inventoryList.map((inventoryObject) => (
+                    <div
+                        key={`deleteItem--${inventoryObject.id}`}
+                        className="inventory__List"
+                        onClick={() => {
+                            if (inventoryObject.quantity > 0) {
+                                sendUserItem(inventoryObject)
+                                    .then(() => {
+                                        return fetchInventory();
+                                    })
+                                    .then((inventoryArray) => {
+                                        setInventoryList(inventoryArray);
+                                    });
+                            }
+                        }}
+                    >
+                        {inventoryObject.name} {inventoryObject.type.nameOfType}
+                        <p className="inventoryListParagraph"></p>
+                        <div id="imgWrapper">
+                            <img id="inventoryImg" src={inventoryObject.picture} />
+                        </div>
+                        {inventoryObject.quantity > 0 ? (
+                            <p>
+                                In Stock <b>{inventoryObject.quantity}</b>
+                            </p>
+                        ) : (
+                            <b>Out of Stock</b>
+                        )}
+                        <article className="buttonContainer">
+                            {localStorage.getItem("inventory__admin") ? (
+                                <button
+                                    id="editButton"
+                                    onClick={() =>
+                                        history.push(`/inventory/${inventoryObject.id}`)
+                                    }
+                                >
+                                    <FontAwesomeIcon icon={faPencil} />
+                                </button>
+                            ) : null}
 
-                            onClick={
-                                () => {
+                            <button
+                                onClick={() => {
                                     if (inventoryObject.quantity > 0) {
                                         sendUserItem(inventoryObject)
                                             .then(() => {
-                                                return fetchInventory()
+                                                return fetchInventory();
                                             })
-                                            .then(inventoryArray => {
-                                                setInventoryList(inventoryArray)
-                                            })
+                                            .then((inventoryArray) => {
+                                                setInventoryList(inventoryArray);
+                                            });
                                     }
-                                }
-                            }>
+                                }}
+                            >
+                                <FontAwesomeIcon icon={faPlus} />
+                            </button>
 
-                            {inventoryObject.name} {inventoryObject.type.nameOfType}
-
-                            <p className="inventoryListParagraph">
-
-                            </p>
-
-                            <div id="imgWrapper">
-                                <img id="inventoryImg" src={inventoryObject.picture} />
-                            </div>
-
-
-                            {inventoryObject.quantity > 0 ? <p>In Stock <b>{inventoryObject.quantity}</b></p> : <b>Out of Stock</b>}
-
-                            <article className="buttonContainer">
-
-                                {localStorage.getItem("inventory__admin") ?
-
-                                    <button
-                                        id="editButton"
-                                        onClick={
-                                            () => history.push(`/inventory/${inventoryObject.id}`)}>
-                                        <FontAwesomeIcon icon={faPencil} />
-
-                                    </button>
-                                    : null
-                                }
-
+                            {localStorage.getItem("inventory__admin") ? (
                                 <button
-                                    onClick={
-                                        () => {
-                                            if (inventoryObject.quantity > 0) {
-                                                sendUserItem(inventoryObject)
-                                                    .then(() => {
-                                                        return fetchInventory()
-                                                    })
-                                                    .then(inventoryArray => {
-                                                        setInventoryList(inventoryArray)
-                                                    })
-                                            }
-                                        }
-                                    }>
-                                    <FontAwesomeIcon icon={faPlus} />
-
+                                    id="deleteButton"
+                                    onClick={() => {
+                                        deleteItem(inventoryObject.id);
+                                    }}
+                                >
+                                    <FontAwesomeIcon icon={faTrash} />
                                 </button>
-
-                                {localStorage.getItem("inventory__admin") ?
-
-                                    <button id="deleteButton"
-                                        onClick={() => {
-                                            deleteItem(inventoryObject.id)
-                                        }}>
-                                        <FontAwesomeIcon icon={faTrash} />
-
-                                    </button>
-                                    : null
-                                }
-
-                            </article>
-
-                        </div>
-                    )
-                }
+                            ) : null}
+                        </article>
+                    </div>
+                ))}
             </div>
-
         </>
-    )
-}
+    );
+};
